c6a1f0364f63d1cc124a2d5562221933fdf426ec
Tommy <zombietommie@gmail.com>
2021-09-19 17:02:44 -0500

    restarted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { AppBar } from "@mui/material";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <AppBar position="fixed">
-                <h2>Tommy's D&D Lookup</h2>
-            </AppBar>
+            <h2>Tommy's D&D Lookup</h2>
         </div>
     );
 }
