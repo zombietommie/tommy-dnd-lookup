41a37674eacf84b064f31a029c05e5665bdd0866
Tommy <zombietommie@gmail.com>
2021-09-20 13:39:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import { Layout, Menu } from "antd";
 import "./App.css";
 
 function App() {
-    const { Header, Content } = Layout;
-    return (
-        <div className="App">
-            <Layout>
-                <Header>
-                    <h2>Tommy's D&D Lookup</h2>
-                    <Menu mode="horizontal">
-                        Menu
-                        <Menu.Item>Menu item</Menu.Item>
-                    </Menu>
-                </Header>
-                <Content>Applle sauce</Content>
-            </Layout>
-        </div>
-    );
+  const { Header, Content } = Layout;
+  return (
+    <div className="App">
+      <Layout>
+        <Header>
+          <h2>Tommy's D&D Lookup</h2>
+          <Menu mode="horizontal">
+            Menu
+            <Menu.Item>Menu item</Menu.Item>
+          </Menu>
+        </Header>
+        <Content>Apple sauce</Content>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
